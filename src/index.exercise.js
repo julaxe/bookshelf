24c21278bf613e10af7c4a1c8c66f16e5b6fce1e
Julaxe <julaxe8@gmail.com>
2021-07-03 13:15:42 -0400

    exercise 1 done without styles

--- src/index.exercise.js
+++ src/index.exercise.js
@@ -1,9 +1,96 @@
-// 🐨 you'll need to import React and ReactDOM up here
 
-// 🐨 you'll also need to import the Logo component from './components/logo'
+import React from 'react'
+import ReactDOM from 'react-dom'
+import {Logo} from './components/logo'
+import {Dialog} from '@reach/dialog'
+import '@reach/dialog/styles.css'
+function stateReducer(state, action){
+    switch(action.type){
+        case 'login':
+            return {login: true, register: false}
+        case 'register':
+            return {login: false, register: true}
+        case 'close':
+            return {login: false, register: false}
+        default:
+            throw new Error('no action in the dispatcher function')
+    }
+}
 
-// 🐨 create an App component here and render the logo, the title ("Bookshelf"), a login button, and a register button.
-// 🐨 for fun, you can add event handlers for both buttons to alert that the button was clicked
 
-// 🐨 use ReactDOM to render the <App /> to the root element
-// 💰 find the root element with: document.getElementById('root')
+function LoginForm({onSubmit, buttonText}){
+    function handleSubmit(event){
+        event.preventDefault()
+        
+        const {username, password} = event.target.elements //it gets the DOM element
+        
+        onSubmit({username: username.value, password: password.value})
+    }
+    return (
+        <div>
+            <form onSubmit={handleSubmit}>
+                <h2>{buttonText}</h2>
+                <div>
+                    <label htmlFor='username' >Username </label>
+                    <input id='username' type='text'></input>
+                </div>
+                <div>
+                    <label htmlFor='password'>Password </label>
+                    <input id='password' type='password'></input>
+                </div>
+                <button type='submit'>{buttonText}</button>
+            </form>
+        </div>
+    )
+}
+
+function App() {
+    const [dialog, dispatcher] = React.useReducer(stateReducer, {login: false, register: false})
+    
+    function handleLogin(event){
+        dispatcher({type:'login'})
+    }
+
+    function handleRegister(event){
+        dispatcher({type:'register'})
+    }
+
+    function handleClose(event){
+        dispatcher({type:'close'})
+        console.log(dialog)
+    }
+
+    function login(formData)
+    {
+        console.log('login',formData)
+    }
+    function register(formData)
+    {
+        console.log('register', formData)
+    }
+
+    return (
+        <div>
+            <h1>Bookshelf</h1>
+            <Logo width='100' height='100'></Logo>
+            <div>
+                <button value="login" onClick={handleLogin}>Login</button>
+                <button value="register" onClick={handleRegister}>Register</button>
+            </div>
+            <Dialog aria-label="Login form" name='loginDialog' isOpen={dialog.login}>
+                <button className="close-button" onClick={() => dispatcher({type: 'close'})}>
+                    <span aria-hidden>×</span>
+                </button>
+                <LoginForm onSubmit={login} buttonText='Login' />
+            </Dialog>
+            <Dialog aria-label="Register form" name='registerDialog' isOpen={dialog.register}>
+                <button className="close-button" onClick={handleClose}>
+                    <span aria-hidden>×</span>
+                </button>
+                <LoginForm onSubmit={register} buttonText='Register' />
+            </Dialog>
+        </div>
+    )
+}
+
+ReactDOM.render(<App />, document.getElementById('root'))
